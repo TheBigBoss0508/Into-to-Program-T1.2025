0e2898cad6cccd3cb898d0144f5a1710da80ae1e
Lahr William <820975@stma.k12.mn.us>
2025-09-29 13:18:23 -0500

    Animal survey

---
+++ Assignments/Animal Survey.py
@@ -0,0 +1,30 @@
+favorite_animal = input("What is your favorite animal?\n> ")
+print("My favorite animal is a " + favorite_animal + ".")
+# Asks whats your favorite animal
+why_like_them = input ("Why do you like them?\n> ")
+print("I like them because they are " + why_like_them + ".")
+# Asks what you like about them
+land_or = input("Does a " + favorite_animal + " live on land or water?\n> ")
+print("A " + favorite_animal + " lives on " + land_or + ".")
+# Asks wheather they live on land or water
+live = input("Where do " + favorite_animal + " live on " + land_or +"?\n> ")
+print("They live in " + live + ".")
+# Asks where they live on land or water
+fun_fact = input("Whats one fun fact about " + favorite_animal + "?\n> ")
+print("One fun fact would be " + fun_fact + ".")
+# Asks one fun fact about them
+color = input("What color is a " + favorite_animal + "?\n> ")
+print("A " + favorite_animal + " is usually " + color)
+# Asks what color your animal is
+seen_one = input("Have you ever seen one?\n> ")
+print(seen_one + ".")
+# Asks if you have ever seen one
+make_noise = input("Does a " + favorite_animal + " make any noise?\n> ")
+print(make_noise + ".")
+# Asks if your animal makes noise
+what_eat = input("What does a " + favorite_animal + " eat?\n> ")
+print("A " + favorite_animal + " usually eats " + what_eat + ".")
+# Asks what your animal eats
+another_animal = input("Do you have another favorite animal?\n> ")
+print(another_animal)
+# Asks if you have any other favorite animals
