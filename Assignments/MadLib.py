099f58d4e4980c5bcc24adb952cb33af2532bf36
Lahr William <820975@stma.k12.mn.us>
2025-09-30 13:49:11 -0500

    Mad lib

---
+++ Assignments/MadLib.py
@@ -0,0 +1,69 @@
+'''
+Once upon a time in a (adjective) land,
+there was a (noun) that gave people potions.
+He had  some (adjective) potions to (adjective) potions,
+there they lives with his (adjective) (noun).
+But it all changed when the (noun) nation (adverb) attacked,
+this attack left many (nouns) servely (adjective).
+But the (noun) had a potion to undo the (adjectice),
+but they also needed to do the secret (noun) (adverb) dance.
+Once they finished the (adjective) dance, the (noun) nation was defeated.
+'''
+
+# Pseudocode
+
+"""
+Type of land = input descibing adjective
+Print user response
+Type of thing = input noun
+Print user response
+Type of potions = input 2 adjectives
+Print user response
+Who the thing lives with = input noun and adjective
+Print user response
+What nation attacked = input noun and adverb
+Print user response
+What happened after the attack = input noun and adjective
+Print user response
+What the potion did = input noun and adjective
+Print user response
+What dance they needed to do = input noun and adverb
+Print user response
+Finished the dance = input adjective and noun
+Print user response
+"""
+
+noun_land = input("Once upon a time in a [Noun] Land,\n> ")
+print = noun_land 
+
+noun_people = input("there was a [Noun] that gave people potions.\n> ")
+print = noun_land
+
+adjective_potion = input("He had some [adjective] potions to (adjective) potions,\n> ")
+print = adjective_potion 
+
+adjective_potion_2 = input("He had some (adjective) potions to [adjective] potions,\n> ")
+print = adjective_potion_2
+
+adjective_live = input("there they lives with his [adjective] (noun).\n> ")
+print = adjective_live
+
+noun_live = input("there they lives with his (adjective) [noun].\n> ")
+print = noun_live
+
+noun_nation = input("But it all changed when the [Noun] nation (adverb) attacked,\n> ")
+print = noun_nation
+
+adverb_nation = input("But it all changed when the (Noun) nation [Adverb] attacked,\n> ")
+print = adverb_nation
+
+noun_attack = input("this attack left many [nouns] servely (adjective).\n> ")
+print = noun_attack 
+
+
+
+noun_undo = input("But the [noun] had a potion to undo the (adjectice),\n> ")
+print = noun_undo
+
+adjective_undo = input("But the (noun) had a potion to undo the [adjectice],\n> ")
+print = adjective_undo
---
+++ Practice/2.5-Practice.py
@@ -0,0 +1,33 @@
+a = 115         #int -> string
+b = 3.14        #float -> string
+c = "68"        #string -> int
+d = "True"      #string -> boolean
+e = True        #boolean -> string
+f = False       #boolean -> string
+g = '10110111'  #byte -> int
+h = "2.54"      #string -> float
+i = 100         #int -> float
+j = 10.0        #float -> int
+k = 254         #int -> byte
+
+print(str(a))
+
+print(str(b))
+
+print(int(c))
+
+print(bool(d))
+
+print(str(e))
+
+print(int(f))
+
+print(int(g))
+
+print(float(h))
+
+print(float(i))
+
+print(int(j))
+
+print(bin(k))
